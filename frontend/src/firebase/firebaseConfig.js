ab7d9acdace41808cb2fd2870274dfe4b626e845
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-09 17:42:42 +0530

    edit frontend

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -1,20 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDT3Ag-JuiIlfO2PrIFQhwifScsY8IxGig",
+//   authDomain: "cloneblog-5396d.firebaseapp.com",
+//   projectId: "cloneblog-5396d",
+//   storageBucket: "cloneblog-5396d.appspot.com",
+//   messagingSenderId: "416249220936",
+//   appId: "1:416249220936:web:24907f94f8c9ca5feded7d",
+//   measurementId: "G-32VSLER2XS",
+//   DatabaseURL: "https://cloneblog-5396d-default-rtdb.firebaseio.com"
+// };
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(import.meta.env.VITE_FIREBASE_API);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDT3Ag-JuiIlfO2PrIFQhwifScsY8IxGig",
-  authDomain: "cloneblog-5396d.firebaseapp.com",
-  projectId: "cloneblog-5396d",
-  storageBucket: "cloneblog-5396d.appspot.com",
-  messagingSenderId: "416249220936",
-  appId: "1:416249220936:web:24907f94f8c9ca5feded7d",
-  measurementId: "G-32VSLER2XS",
-  DatabaseURL: "https://cloneblog-5396d-default-rtdb.firebaseio.com"
+  apiKey: import.meta.env.VITE_FIREBASE_API,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+  DatabaseURL: import.meta.env.VITE_FIREBASE_URL
 };
 
 // Initialize Firebase
@@ -22,3 +43,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export {app};
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export {app};
