0285b80da63033f3b48e5cddeb9ccea9930088d2
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-10 20:59:17 +0530

    17th commit

--- backend/dist/controller/messages.js
+++ backend/dist/controller/messages.js
@@ -134,6 +134,7 @@ function sendContacts(req, res) {
         if (user) {
             const socket = socket_1.default.getIO();
             const userSocket = socket.sockets.sockets.get(socketid);
+            console.log(userSocket);
             yield (userSocket === null || userSocket === void 0 ? void 0 : userSocket.join(hash));
             console.log(userSocket === null || userSocket === void 0 ? void 0 : userSocket.rooms, "hash 125 sendcontacts");
         }
