e203a5c2d8309a1f7447e2adfa2417d1763a37f2
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-12 16:57:39 +0530

    20th commit

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -75,7 +75,7 @@ catch(err){
         headers: {
             'Content-Type': 'application/json'
         }})
-        console.log(res.data);
+        console.log(res.data,"logout");
         navigate('/login')
         dispatch(logout())
     }} className='text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>Logout</button>
