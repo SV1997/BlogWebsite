ee99141ea5480749ddbf6c42de5ce445cc3a73c8
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-14 18:30:22 +0530

    23rd frontend commit

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -76,7 +76,8 @@ catch(err){
             'Content-Type': 'application/json'
         }})
         console.log(res.data,"logout");
-        sessionStorage.clear()
+        sessionStorage.removeItem('userInfo')
+        sessionStorage.removeItem('socketId')
         navigate('/login')
         dispatch(logout())
     }} className='text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>Logout</button>
