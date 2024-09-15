054f168ed39754917dcd9182c76e5f29f535c333
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-15 19:48:24 +0530

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,16 +32,16 @@ app.use(cookieParser(secretKey));
 //   );
 
 // Middleware Setup
-app.options('https://blog-website-75t9nj6vw-saharsh-vahsishthas-projects.vercel.app', cors());  // Include before your other routes
+app.options('https://blog-website-c2rt6u6ig-saharsh-vahsishthas-projects.vercel.app', cors());  // Include before your other routes
 
 app.use(cors({
-    origin: 'https://blog-website-75t9nj6vw-saharsh-vahsishthas-projects.vercel.app', // This should be the URL of your frontend
+    origin: 'https://blog-website-c2rt6u6ig-saharsh-vahsishthas-projects.vercel.app', // This should be the URL of your frontend
     credentials: true, // To allow cookies to be shared between backend and frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowable methods
     allowedHeaders: ['Content-Type', 'Authorization'],
 })); 
 app.use((req:Request, res:Response, next:NextFunction) => {
-    res.header('Access-Control-Allow-Origin', 'https://blog-website-75t9nj6vw-saharsh-vahsishthas-projects.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://blog-website-c2rt6u6ig-saharsh-vahsishthas-projects.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
