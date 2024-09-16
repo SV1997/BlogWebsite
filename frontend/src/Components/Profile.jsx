f61917d14f19f8344f478dc853ac27a001ab224b
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-16 18:47:15 +0530

    changes

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -80,7 +80,10 @@ catch(err){
         sessionStorage.clear()
         const user=sessionStorage.getItem('userInfo')
         console.log(user);
+        console.log(socket,"before");
         socket.disconnect();
+        console.log(socket,"socket after");
+        
         dispatch(logout())
         navigate('/login')
     }} className='text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>Logout</button>
