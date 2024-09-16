64e5ae8e4a781121db56808822386732c82e67f2
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-16 14:53:31 +0530

    changes

--- backend/dist/socket.js
+++ backend/dist/socket.js
@@ -6,7 +6,7 @@ const server = {
     init: (server) => {
         io = new socket_io_1.Server(server, {
             cors: {
-                origin: 'https://blog-website-p12mz2hi5-saharsh-vahsishthas-projects.vercel.app',
+                origin: 'blog-website-git-master-saharsh-vahsishthas-projects.vercel.app',
                 methods: ["GET", "POST"],
                 allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "X-CSRF-Token", "Access-Control-Allow-Origin"],
                 credentials: true
