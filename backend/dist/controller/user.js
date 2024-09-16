818d91d02cddf310ae14e513f91c5cd3853b5fe2
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-16 16:18:10 +0530

    changes

--- backend/dist/controller/user.js
+++ backend/dist/controller/user.js
@@ -386,6 +386,9 @@ const logout = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             socket.emit("disconnect", "disconnected");
             socket.disconnect(true);
         }
+        req.session.destroy((err) => {
+            console.log(err);
+        });
         res.status(200).json({ message: 'Logged out successfully' });
     }
     catch (error) {
