8d78abc2f6c1151b04aa9a725b06cf09353a744a
saharsh vashishtha <saharsh.vashishtha@gmail.com>
2024-09-17 08:17:51 +0530

    socket changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,73 @@
-import { useEffect, useState } from 'react'
-import Navigation from './Components/Sidebar/Navigation'
-import './App.css'
-import { useLocation } from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
-import Trending from './Components/Trendings/Trending'
-import ResponsiveButtons from './Components/ResponsiveButtons'
-import useSocket from './Components/useSocket'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import Navigation from './Components/Sidebar/Navigation';
+import './App.css';
+import { useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Trending from './Components/Trendings/Trending';
+import ResponsiveButtons from './Components/ResponsiveButtons';
+import useSocket from './Components/useSocket';
+import axios from 'axios';
+
 function App() {
-  // const [page, setPage] = useState('')
-  const socket=useSocket()
+  const socket = useSocket();
   console.log(socket);
-  
-    const handleWindowChange = async (event) => {
-      event.preventDefault();
+
+  const handleWindowChange = async (event) => {
+    event.preventDefault();
+    if (socket) {
       socket.disconnect();
-      navigator.sendBeacon('https://shark-app-ahkas.ondigitalocean.app/api/v1/user/windowleave');
-      console.log(res, 'unload');
     }
+    navigator.sendBeacon('https://shark-app-ahkas.ondigitalocean.app/api/v1/user/windowleave');
+    console.log('Window unload event');
+  };
 
-    useEffect(() => {
-      window.addEventListener('beforeunload', handleWindowChange);
-      return () => window.removeEventListener('beforeunload', handleWindowChange);
-    }, []);
-const location= useLocation();
-const isLogin= location.pathname==='/login' 
-const isSignup= location.pathname==='/signup'
-const socketId= socket.on('socketId',(data)=>{
-  sessionStorage.setItem('socketId', data);
-  console.log(data);
-} )
-if(!(isLogin||isSignup)){
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleWindowChange);
+    return () => window.removeEventListener('beforeunload', handleWindowChange);
+  }, [socket]);
+
+  const location = useLocation();
+  const isLogin = location.pathname === '/login';
+  const isSignup = location.pathname === '/signup';
+
+  useEffect(() => {
+    if (socket) {
+      const socketIdHandler = (data) => {
+        sessionStorage.setItem('socketId', data);
+        console.log('Received socketId:', data);
+      };
+
+      socket.on('socketId', socketIdHandler);
+
+      // Cleanup the event listener on unmount or when socket changes
+      return () => {
+        socket.off('socketId', socketIdHandler);
+      };
+    }
+  }, [socket]);
+
+  if (!(isLogin || isSignup)) {
+    return (
+      <div className="flex lg:flex-row">
+        <div className="flex flex-col w-1/4">
+          <Navigation />
+        </div>
+        <div className="border-gray-800 flex-col w-1/2 border-r-2 border-l-2 border-b-2 h-full">
+          <ResponsiveButtons />
+          <Outlet />
+        </div>
+        <div className="md:flex p-1/2 w-1/2 flex-col sd:w-1/4 hidden">
+          <Trending />
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className='flex lg:flex-row '>
-      <div className='flex flex-col w-1/4'>
-    <Navigation/>
+    <div className="flex justify-center align-center">
+      <Outlet />
     </div>
-    <div className='border-gray-800 flex-col w-1/2 border-r-2 border-l-2 border-b-2 h-full'>
-    <ResponsiveButtons/>
-    <Outlet />
-    </div>
-    <div className='md:flex p-1/2 w-1/2 flex-col sd:w-1/4 hidden'>
-    <Trending/>
-    </div>
-    </div>
-  )
-}
-return(
-  <div className='flex justify-center align-center'>
-    <Outlet/>
-  </div>
-)
+  );
 }
 
-export default App
+export default App;
